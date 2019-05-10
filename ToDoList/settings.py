fa9c767d9469a1a1699f71d3b7a411ce7aadc03a
Beakjiyeon <bjy1530@naver.com>
2019-05-11 00:00:59 +0900

    앱 등록, 템플릿 index.html 생성, url 정리 연결, board앱에 urls.py 생성

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'board.apps.BoardConfig', # board 앱 등록
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- board/urls.py
+++ board/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('board.urls')),
+    path('index',views.index,name='index'), # 첫 화면
    
 ]
